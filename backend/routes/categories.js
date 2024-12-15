ddd084b906ea7841bfaba863522537a4e4ea2c70
Spiridonova Nataliia <spiridonova.nataliia@gmail.com>
2024-12-15 03:41:01 +0300

    api для получения списка категорий (ид+название)

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -14,4 +14,24 @@ router.post('/', async (req, res) => {
   }
 });
 
+// Получение списка категорий - для списка в форме
+router.get('/categories', async (req, res) => {
+  try {
+    // все категории из коллекции
+    const categories = await db.getDb().collection('categories').find({}, { projection: { name: 1 } }).toArray();
+
+    // Преобразование _id из ObjectId в строку и вернуть ид и название
+    const formattedCategories = categories.map(category => ({
+      _id: category._id.toString(), // Преобразование ObjectId в строку
+      name: category.name,
+    }));
+
+    // Возвращение массива категорий
+    res.status(200).json(formattedCategories);
+  } catch (error) {
+    console.error('Error fetching categories:', error);
+    res.status(500).json({ error: 'Failed to fetch categories' });
+  }
+});
+
 module.exports = router;
